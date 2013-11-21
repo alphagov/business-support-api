399acffd4200abfb0c069e8a59ed92865704c12a
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-11-21 17:14:48 +0000

    $ rails new business-support-api --skip-active-record --skip-javascript --skip-test-unit --skip-sprockets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusinessSupportApi::Application.config.secret_token = '054d9d8413aaea5bebbf96a55ad17a3e3739378337d79c393eef7c836047aa079084828a9b476f14465eb7785489a1bccebcbc7e3a2ddd52e513c104af711d42'
