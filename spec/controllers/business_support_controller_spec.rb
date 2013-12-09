12b71aefb366176ffaf4be6a63f6572fd7a1d7e1
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-12-09 14:05:36 +0000

    Add controller specs

--- spec/controllers/business_support_controller_spec.rb
+++ spec/controllers/business_support_controller_spec.rb
@@ -19,4 +19,22 @@ describe BusinessSupportController do
       response.status.should == 406
     end
   end
+
+  describe "GET show" do
+    it "should set expiry headers" do
+      get :show, :slug => "foo", :format => :json
+      response.headers["Cache-Control"].should == "max-age=1800, public"
+    end
+
+    it "should accept json requests" do
+      get :show, :slug => "bar", :format => :json
+      response.should be_success
+      response.header['Content-Type'].should include 'application/json'
+    end
+
+    it "should deny other formats" do
+      get :show, :slug => "foo"
+      response.status.should == 406
+    end
+  end
 end
