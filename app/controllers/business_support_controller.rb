0a3aa080460c6cb408884e14b96cf00e75524666
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-01-13 15:48:31 +0000

    Add paginated result set presenter
    Moves a lot of the pagination view logic into one place.

--- app/controllers/business_support_controller.rb
+++ app/controllers/business_support_controller.rb
@@ -4,10 +4,9 @@ class BusinessSupportController < ApplicationController
 
   def search
     @schemes = Scheme.lookup(filtered_params)
-    @schemes.populate_page_links { |page_number| pagination_url(page_number) }
 
     respond_to do |format|
-      format.json
+      format.json { render json: PaginatedResultSetPresenter.new(@schemes, view_context) }
     end
   end
 
@@ -24,22 +23,6 @@ class BusinessSupportController < ApplicationController
   def filtered_params
     valid_keys = Scheme::FACET_KEYS + [:page_number, :page_size]
     params.select { |k,v| valid_keys.include?(k.to_sym) }.symbolize_keys
-  end
-
-  # Returns a pagination url to be used in the JSON response.
-  #
-  # When requested through the public_api endpoint (distunguised by the
-  # presence of the `API-PREFIX` HTTP header), this is the public API URL.
-  #
-  # For other requests, this is the business-support-schemes URL.
-  def pagination_url(page_number)
-    api_prefix = request.headers["HTTP_API_PREFIX"]
-    if api_prefix.present?
-      path_with_query = url_for(params.merge({:page_number => page_number, :only_path => true}))
-      "#{Plek.current.website_root}/#{api_prefix}#{path_with_query}"
-    else
-      url_for(params.merge({:page_number => page_number}))
-    end
   end
 
 end
