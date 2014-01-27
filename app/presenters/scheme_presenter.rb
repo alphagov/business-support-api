4a094cb121589277987aaa413504691fffa808ce
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-01-27 13:19:21 +0000

    Use url helper in scheme presenter

--- app/presenters/scheme_presenter.rb
+++ app/presenters/scheme_presenter.rb
@@ -1,25 +1,14 @@
 class SchemePresenter
 
-  def initialize(scheme, view_context)
+  def initialize(scheme, url_helper)
     @scheme = scheme
-    @view_context = view_context
+    @url_helper = url_helper
   end
 
   def as_json(options={})
     attrs = @scheme.marshal_dump
-    attrs[:id] = url_for_id(attrs[:web_url])
+    attrs[:id] = @url_helper.build_scheme_url(attrs[:id])
     valid_keys = [:id, :identifier, :title, :details, :web_url, :priority] + Scheme::FACET_KEYS
     attrs.select { |k,v| valid_keys.include?(k.to_sym) }
-  end
-
-  def url_for_id(url)
-    api_prefix = @view_context.request.headers["HTTP_API_PREFIX"]
-    uri = URI(url)
-    slug = uri.path.split('/').last
-    if api_prefix
-      "#{Plek.current.website_root}/#{api_prefix}#{@view_context.url_for(format: :json, slug: slug, only_path: true)}"
-    else
-      @view_context.scheme_url(format: :json, slug: slug)
-    end
   end
 end
