4cc4f3bc079cbe71b3337226257fc3af07077a28
issyl0 <isabell@issyl0.co.uk>
2015-02-27 16:56:07 +0000

    Rails 4 deprecates config.whiny_nils in favour of config.eager_load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ BusinessSupportApi::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  config.eager_load = true
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
